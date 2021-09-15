6d638afae0a239c9a2487bfb7976dc0f2cc03aed
Eliud <58745656+eliudio@users.noreply.github.com>
2021-09-15 09:05:46 +0100

    Improvements to creator

--- lib/frontend/impl/mona_list_tile_impl.dart
+++ lib/frontend/impl/mona_list_tile_impl.dart
@@ -16,4 +16,24 @@ class MonaListTileImpl implements HasListTile {
       subtitle: subtitle,
     );
   }
+
+  @override
+  Widget radioListTile<T>(BuildContext context, T t, T? groupValue,
+      String title, String subTitle, ValueChanged<T?>? valueChanged) {
+    return RadioListTile(
+        value: t,
+        groupValue: groupValue,
+        title: Text(title,),
+        subtitle: Text(subTitle),
+        onChanged: valueChanged);
+  }
+
+  @override
+  Widget checkboxListTile(BuildContext context, String title, bool? value,
+      ValueChanged<bool?>? onChanged) {
+    return CheckboxListTile(
+        title: Text(title,),
+        value: value,
+        onChanged: onChanged);
+  }
 }
